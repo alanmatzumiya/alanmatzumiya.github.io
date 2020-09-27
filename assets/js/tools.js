d8ba805b72022d2807999bd45b1591b8c11a9cbc
Alan Matzumiya <alan.matzumiya@outlook.es>
2020-09-27 03:51:13 -0700

    Update tools.js

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -6,18 +6,35 @@ request.send();
 
 request.onload = function() {
   const jsonObj = request.response;
-  var desc = jsonObj['description'];
-  var trad = jsonObj['traduction'];
-  showDescript();
+  document.getElementById('desc-git').innerHTML = jsonObj['description'];
+  document.getElementById("desc-button").innerHTML = "<button onclick='showTrad()' style='float: right;'>Read in Spanish</button>";
 }
 
 function showDescript() {
-  document.getElementById('desc-git').innerHTML = desc;
-  document.getElementById("desc-button").innerHTML = "<button onclick='showButton()' style='float: right;'>Read in Spanish</button>";
-}	
+  let requestURL = 'https://raw.githubusercontent.com/alanmatzumiya/alanmatzumiya.github.io/gh-pages/data.json';
+  let request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = function() {
+  const jsonObj = request.response;
+  document.getElementById('desc-git').innerHTML = jsonObj['description'];
+  document.getElementById("desc-button").innerHTML = "<button onclick='showTrad()' style='float: right;'>Read in Spanish</button>";
+  }
+}			
 
 function showTrad() {
-  textTrad.innerHTML = jsonObj['traduction'];
-  document.getElementById('desc-git').innerHTML = trad;
-  document.getElementById("desc-button").innerHTML = "<button onclick='showDescript()' style='float: right;'>Leer en Ingles</button>";
+  let requestURL = 'https://raw.githubusercontent.com/alanmatzumiya/alanmatzumiya.github.io/gh-pages/data.json';
+  let request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = function() {
+  const jsonObj = request.response;
+  document.getElementById('desc-git').innerHTML = jsonObj['traduction'];
+  document.getElementById("desc-button").innerHTML = "<button onclick='showDescript()' style='float: right;'>Read in English</button>";
+  }
+
 }
