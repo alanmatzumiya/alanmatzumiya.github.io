4b888b87f683385faba3078427d4c138f2207cf7
Alan Matzumiya <alan.matzumiya@outlook.es>
2020-09-27 03:10:08 -0700

    Update tools.js

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,27 +1,23 @@
+let requestURL = 'https://raw.githubusercontent.com/alanmatzumiya/alanmatzumiya.github.io/gh-pages/data.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
 request.onload = function() {
-  showDescript();
+  const jsonObj = request.response;
+  var desc = jsonObj['description'];
+  var trad = jsonObj['traduction'];
+  showDescript(descText);
 }
 
 function showDescript() {
-  let requestURL = 'https://raw.githubusercontent.com/alanmatzumiya/alanmatzumiya.github.io/gh-pages/data.json';
-  let request = new XMLHttpRequest();
-  request.open('GET', requestURL);
-  request.responseType = 'json';
-  request.send();
-  const jsonObj = request.response;
-  const textDesc = document.getElementById('desc-git');
-  textDesc.innerHTML = jsonObj['description'];
-  document.getElementById("desc-button").innerHTML = "<button onclick='tradButton()' style='float: right;'>Traducir a Español</button>";
+  document.getElementById('desc-git').innerHTML = desc;
+  document.getElementById("desc-button").innerHTML = "<button onclick='showButton()' style='float: right;'>Traducir a Español</button>";
 }	
 
-function tradButton() {
-  let requestURL = 'https://raw.githubusercontent.com/alanmatzumiya/alanmatzumiya.github.io/gh-pages/data.json';
-  let request = new XMLHttpRequest();
-  request.open('GET', requestURL);
-  request.responseType = 'json';
-  request.send();
-  const jsonObj = request.response;
-  const textTrad = document.getElementById('desc-git');
+function showTrad() {
   textTrad.innerHTML = jsonObj['traduction'];
+  document.getElementById('desc-git').innerHTML = trad;
   document.getElementById("desc-button").innerHTML = "<button onclick='showDescript()' style='float: right;'>Traducir a Ingles</button>";
 }
