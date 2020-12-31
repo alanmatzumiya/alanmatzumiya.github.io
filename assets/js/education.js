92d40b02dbc035ff74c84d44882ad265fa69a105
Alan Matzumiya <alan.matzumiya@outlook.es>
2020-12-30 23:35:13 -0700

    Update education.js

--- assets/js/education.js
+++ assets/js/education.js
@@ -10,11 +10,21 @@ requestThesis.onload = function() {
   const textMath = jsonThesis['thesisMath'];
   document.getElementById('author-math').innerHTML = textMath['author'];
   document.getElementById('date-math').innerHTML = textMath['date'];
-  document.getElementById('url-math').innerHTML = "<a href="+textMath['url']+" target='_blank'>Thesis</a>";
+  document.getElementById('url-math').innerHTML = "<a href="+textMath['url']+" target='_blank'>Thesis Document</a>";
   document.getElementById('description-math').innerHTML = textMath['description'];
   document.getElementById('sponsorship-math').innerHTML = textMath['sponsorship'];
   document.getElementById('publisher-math').innerHTML = textMath['publisher'];
   document.getElementById('classification-math').innerHTML = textMath['classification'];
   document.getElementById('title-math').innerHTML = textMath['title'];
   document.getElementById('type-math').innerHTML = textMath['type'];
+  const textChemistry = jsonThesis['thesisChemistry'];
+  document.getElementById('author-chemistry').innerHTML = textChemistry['author'];
+  document.getElementById('date-chemistry').innerHTML = textChemistry['date'];
+  document.getElementById('url-chemistry').innerHTML = "<a href="+textChemistry['url']+" target='_blank'>Thesis Document</a>";
+  document.getElementById('description-chemistry').innerHTML = textMath['description'];
+  document.getElementById('sponsorship-chemistry').innerHTML = textChemistry['sponsorship'];
+  document.getElementById('publisher-chemistry').innerHTML = textChemistry['publisher'];
+  document.getElementById('classification-chemistry').innerHTML = textChemistry['classification'];
+  document.getElementById('title-chemistry').innerHTML = textChemistry['title'];
+  document.getElementById('type-chemistry').innerHTML = textChemistry['type'];
 }
