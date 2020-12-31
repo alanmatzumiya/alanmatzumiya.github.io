1f31bea529c8c88c726382f7317831701490f0bc
Alan Matzumiya <alan.matzumiya@outlook.es>
2020-12-30 22:36:47 -0700

    Update education.js

--- assets/js/education.js
+++ assets/js/education.js
@@ -1,29 +1,11 @@
-(function() {
-    var updateButton1 = document.getElementById('degree');
-    var cancelButton1 = document.getElementById('cancelDegree');
-    var favDialog1 = document.getElementById('thesisDegree');
-    var updateButton2 = document.getElementById('bachelor');	  
-    var cancelButton2 = document.getElementById('cancelBachelor'); 
-    var favDialog2 = document.getElementById('thesisBachelor');
-    
-    // Update button opens a modal dialog
-    updateButton1.addEventListener('click', function() {
-      favDialog1.showModal();
-    });
 
-    // Form cancel button closes the dialog box
-    cancelButton1.addEventListener('click', function() {
-      favDialog1.close();
-    });
-	  
-    // Update button opens a modal dialog
-    updateButton2.addEventListener('click', function() {
-      favDialog2.showModal();
-    });
+let urlFile = 'https://raw.githubusercontent.com/alanmatzumiya/alanmatzumiya.github.io/gh-pages/assets/projects.json';
+let requestThesis = new XMLHttpRequest();
+requestThesis.open('GET', urlFile);
+requestThesis.responseType = 'json';
+requestThesis.send();
 
-    // Form cancel button closes the dialog box
-    cancelButton2.addEventListener('click', function() {
-      favDialog2.close();
-    });
-	  
-  })();
+requestProjects.onload = function() {
+  const jsonThesis = requestThesis.response;
+  document.getElementById('author').innerHTML = jsonProjects['thesis'][1]['author'];
+}
