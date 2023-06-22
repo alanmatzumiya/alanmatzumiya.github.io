f431f7e0e200d245bfebc32ff677354b3bb1310e
alanmatzumiya <alan.matzumiya@outlook.es>
2023-06-22 12:14:55 -0700

    autocommit

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@
 
 from werkzeug.exceptions import HTTPException
 from build import Server
-from views.build import Template
+from views.build import Template, request, json
 server = Server()
 app = server.app
 
@@ -10,6 +10,15 @@ app = server.app
 @app.errorhandler(HTTPException)
 def handle_exception(error):
     return Template().error404(error)
+
+
+@app.route("/get-data/")
+def getdata():
+    req = request.args.to_dict()
+    headers = dict(request.headers)
+    config = dict(app.config)
+    config.pop("PERMANENT_SESSION_LIFETIME")
+    return Template.send_json(req, config=config, headers=headers)
 
 
 if __name__ == "__main__":
---
+++ app/job-searcher.py
@@ -0,0 +1,34 @@
+from webbrowser import open_new_tab
+
+
+class WebSite:
+    names = (
+        "indeed",
+        "bumeran",
+        "occ",
+        "computrabajo",
+        "linkedin"
+    )
+    urls = (
+        "https://mx.indeed.com/",
+        "https://www.bumeran.com.mx/",
+        "https://www.occ.com.mx/",
+        "https://mx.computrabajo.com/",
+        "https://www.linkedin.com/"
+    )
+    page = {name: url for name, url in zip(names, urls)}
+
+    @staticmethod
+    def geturl(name):
+        return WebSite.page.get(name)
+
+    @staticmethod
+    def openurl(name):
+        open_new_tab(WebSite.geturl(name))
+
+    @staticmethod
+    def open_all():
+        for url in WebSite.urls:
+            open_new_tab(url)
+
+WebSite.open_all()
